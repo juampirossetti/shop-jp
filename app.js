4033b953d7c6f9ff238c0599c6cb31ba9d9da2e8
Juampi R <juampirossetti@gmail.com>
2019-05-31 17:29:39 -0300

    Emails, validations, error handling

--- app.js
+++ app.js
@@ -3,23 +3,51 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const mongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+const flash = require('connect-flash');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI = 'mongodb+srv://node-complete:node-complete@nodejs-1qztw.mongodb.net/test';
+
 const app = express();
+const store = new mongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions',
+});
+
+const csrfProtection = csrf();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  }),
+);
+
+app.use(csrfProtection);
+app.use(flash());
+
 app.use((req, res, next) => {
-  User.findById('5ce812e36607ec308bb9acdb')
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then(user => {
       req.user = user;
       next();
@@ -27,30 +55,22 @@ app.use((req, res, next) => {
     .catch(err => console.log(err));
 });
 
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    'mongodb+srv://node-complete:node-complete@nodejs-1qztw.mongodb.net/test?retryWrites=true',
-  )
+  .connect(MONGODB_URI)
   .then(result => {
-    User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-          name: 'Juan',
-          email: 'juanpablo@girosit.com',
-          cart: {
-            items: [],
-          },
-        });
-        user.save();
-      }
-      console.log('Connected');
-      app.listen(3000);
-    });
+    app.listen(3000);
   })
   .catch(err => {
     console.log(err);
